0f3f19551b975a5f5b6aa856847b2585c7357635
Meg-Lander <chertousov-vova@ya.ru>
2025-05-30 01:40:41 +0300

    (api service) feat: initial configuration for the ui and framework usage

---
+++ AkinatorLogicService/AkinatorApi/Migrations/20250517233111_AddSessions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AkinatorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSessions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CharacterName = table.Column<string>(type: "TEXT", nullable: true),
+                    StartedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
---
+++ AkinatorLogicService/AkinatorApi/Migrations/20250518000756_AddAnswers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AkinatorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnswers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SessionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    QuestionNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    AnswerText = table.Column<string>(type: "TEXT", nullable: false),
+                    AnsweredAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+        }
+    }
+}
