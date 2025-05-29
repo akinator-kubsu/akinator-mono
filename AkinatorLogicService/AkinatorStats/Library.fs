692740ae66062b6c1ea8ea0c9d1b83cc3636d86f
Artem-Darius Weber <mit.3tlasa@gmail.com>
2025-05-30 01:58:44 +0300

    (lib stats) fix: DB connection and logging added

--- AkinatorLogicService/AkinatorStats/Library.fs
+++ AkinatorLogicService/AkinatorStats/Library.fs
@@ -2,19 +2,35 @@
 
 open System
 open Microsoft.Data.Sqlite
+open Serilog
+open Serilog.Events
 
 module Stats =
     
     let connectionString = "Data Source=akinator.db"
+    
+    let private logger = 
+        LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/stats.log", rollingInterval = RollingInterval.Day)
+            .CreateLogger()
+
+    let private executeQuery query =
+        try
+            use connection = new SqliteConnection(connectionString)
+            connection.Open()
+            use command = new SqliteCommand(query, connection)
+            command.ExecuteScalar() :?> int64 |> int
+        with
+        | ex -> 
+            logger.Error(ex, "Ошибка при выполнении запроса: {Query}", query)
+            raise ex
 
     let getUserCount () =
-        use connection = new SqliteConnection(connectionString)
-        connection.Open()
-        use command = new SqliteCommand("SELECT COUNT(*) FROM Users", connection)
-        command.ExecuteScalar() :?> int64 |> int
+        logger.Debug("Получение количества пользователей")
+        executeQuery "SELECT COUNT(*) FROM Users"
 
     let getSessionCount () =
-        use connection = new SqliteConnection(connectionString)
-        connection.Open()
-        use command = new SqliteCommand("SELECT COUNT(*) FROM Sessions", connection)
-        command.ExecuteScalar() :?> int64 |> int
+        logger.Debug("Получение количества сессий")
+        executeQuery "SELECT COUNT(*) FROM Sessions"
