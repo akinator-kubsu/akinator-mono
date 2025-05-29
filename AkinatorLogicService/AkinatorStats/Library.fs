b0fca1fe4e877e613c3fec262fb60cdec82029dd
Sergey Dudo <dudo@mail.ru>
2025-05-30 01:57:30 +0300

    (lib stats) feat: Получение кол-ва сессий и пользователей

---
+++ AkinatorLogicService/AkinatorStats/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace AkinatorStats
+
+open System
+open Microsoft.Data.Sqlite
+
+module Stats =
+    
+    let connectionString = "Data Source=akinator.db"
+
+    let getUserCount () =
+        use connection = new SqliteConnection(connectionString)
+        connection.Open()
+        use command = new SqliteCommand("SELECT COUNT(*) FROM Users", connection)
+        command.ExecuteScalar() :?> int64 |> int
+
+    let getSessionCount () =
+        use connection = new SqliteConnection(connectionString)
+        connection.Open()
+        use command = new SqliteCommand("SELECT COUNT(*) FROM Sessions", connection)
+        command.ExecuteScalar() :?> int64 |> int
