574956f03e75b3d1bd445fadcf4772a8199b27d2
Artem-Darius Weber <mit.3tlasa@gmail.com>
2025-05-30 01:42:51 +0300

    (api service) feat: added sentry logging and tracing

---
+++ AkinatorApiService/Program.cs
@@ -0,0 +1,65 @@
+using AkinatorWeb.Services;
+using AkinatorWeb.Mapping;
+using Sentry;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+builder.Services.AddSession();
+
+// Add database service
+builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
+
+// Add user service
+builder.Services.AddScoped<IUserService, UserService>();
+
+// Add session service
+builder.Services.AddScoped<ISessionService, SessionService>();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Add Sentry
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = "https://d31fac0ec6335ec33e8972670e74fcf0@o4509378568454144.ingest.us.sentry.io/4509378570420224";
+    o.Debug = true;
+    o.TracesSampleRate = 1.0;
+    o.Environment = builder.Environment.EnvironmentName;
+    o.MinimumBreadcrumbLevel = LogLevel.Debug;
+    o.MinimumEventLevel = LogLevel.Warning;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
+
+app.UseStaticFiles();
+app.UseSession();
+app.UseRouting();
+app.UseAuthorization();
+
+// Добавляем Sentry middleware
+app.UseSentryTracing();
+
+// Тестовое логирование
+SentrySdk.CaptureMessage("Application started", SentryLevel.Info);
+
+app.MapGet("/", context =>
+{
+    SentrySdk.CaptureMessage("Home page accessed", SentryLevel.Info);
+    context.Response.Redirect("/Login");
+    return Task.CompletedTask;
+});
+
+app.MapRazorPages();
+
+app.Run();
